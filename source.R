6c62c5de44874d0a73a2fcd37599907cf1563c43
Sam <249195@student.pwr.edu.pl>
2026-04-20 15:31:51 +0200

    first commit

---
+++ source.R
@@ -0,0 +1,28 @@
+############### read data from the CSV file ###############
+
+df <- read.csv("student_performance/student/student-por.csv", sep = ";")
+
+
+############### decode variables ###############
+
+# categorical
+
+factor_vars <- c(
+  "school","sex","address","famsize","Pstatus",
+  "Mjob","Fjob","reason","guardian",
+  "schoolsup","famsup","paid","activities",
+  "nursery","higher","internet","romantic"
+)
+
+df[factor_vars] <- lapply(df[factor_vars], as.factor)
+
+
+# add binary variable for pass/fail
+
+df$pass <- ifelse(df$G3 >= 10, "pass", "fail")
+df$pass <- as.factor(df$pass)
+
+
+
+str(df)
+summary(df)
